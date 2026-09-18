90d398851628d25b39e58ea6a94185453c71d548
sreerag <sreerag7460@gmail.com>
2026-09-18 05:39:21 +0530

    Fix SK logo: replace transparent PNG with inline SVG, bump cache v6

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Cache-first for static assets (images, audio, THREE.js)
 // Network-first for HTML (so updates are picked up quickly)
 
-var CACHE_VERSION = 'portfolio-v5';
+var CACHE_VERSION = 'portfolio-v6';
 var STATIC_CACHE = CACHE_VERSION + '-static';
 var IMAGE_CACHE  = CACHE_VERSION + '-images';
 
